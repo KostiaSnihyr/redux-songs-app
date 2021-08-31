33993a44e8b3f2f5d81a04aaa8ce74fa73f6191f
Kostiantyn <kosssm155@gmail.com>
2021-08-31 19:30:05 +0300

    connect(mapStateToProps)(SongList)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import actions from '../actions';
-import { selectSong } from '../actions';
+import SongList from './SongList';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<SongList />
+		</div>
+	);
 };
 
 export default App;
